679b31623c555033125279971cf0b2a0f604f558
Mikołaj Marciniak <mikolaj.marciniak04@gmail.com>
2026-02-27 15:24:34 +0100

    Update schema and add indexes

--- db/schema.sql
+++ db/schema.sql
@@ -25,9 +25,8 @@ CREATE TABLE IF NOT EXISTS team (
 --RACES
 CREATE TABLE IF NOT EXISTS race (
     _id SERIAL PRIMARY KEY,
-    slug TEXT NOT NULL UNIQUE,
-    season INTEGER NOT NULL,
-    round INTEGER NOT NULL,
+    season INTEGER NOT NULL CHECK (season >= 1950),
+    round INTEGER NOT NULL CHECK (round >= 1),
     fk_circuit_id INTEGER NOT NULL,
 
     CONSTRAINT fk_circuit_id_constraint
@@ -38,14 +37,17 @@ CREATE TABLE IF NOT EXISTS race (
     CONSTRAINT uq_race_season_round UNIQUE (season, round)
 );
 
+--RACES-INDEXES
+CREATE INDEX IF NOT EXISTS idx_race_fk_circuit_id ON race(fk_circuit_id);
+
 --RESULTS
 CREATE TABLE IF NOT EXISTS result (
     _id SERIAL PRIMARY KEY,
-    fk_race_id INT NOT NULL,
-    fk_driver_id INT NOT NULL,
-    fk_team_id INT NOT NULL,
-    position INT NOT NULL,
-    points NUMERIC(5, 2) NOT NULL,
+    fk_race_id INTEGER NOT NULL,
+    fk_driver_id INTEGER NOT NULL,
+    fk_team_id INTEGER NOT NULL,
+    position INTEGER NOT NULL CHECK (position >= 1),
+    points NUMERIC(5, 2) NOT NULL CHECK (points >= 0),
 
     CONSTRAINT fk_race_id_constraint
         FOREIGN KEY (fk_race_id)
@@ -64,3 +66,8 @@ CREATE TABLE IF NOT EXISTS result (
 
     CONSTRAINT uq_result_race_driver UNIQUE (fk_race_id, fk_driver_id)
 );
+
+--RESULTS-INDEXES
+CREATE INDEX IF NOT EXISTS idx_result_fk_race_id ON result(fk_race_id);
+CREATE INDEX IF NOT EXISTS idx_result_fk_driver_id ON result(fk_driver_id);
+CREATE INDEX IF NOT EXISTS idx_result_fk_team_id ON result(fk_team_id);
